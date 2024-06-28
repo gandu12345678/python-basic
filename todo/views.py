84fd52140d7deede2cdcba7eacdaaad72665d56f
shisimanu <kunwarsamriddha@gmail.com>
2024-06-28 15:20:28 +0545

    yup

--- todo/views.py
+++ todo/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    people=[
+        {"name":"ram","age":"2"},
+        {"name":"shyam","age":"3"},
+        {"name":"john","age":"4"},
+
+        
+    ]
+    
+    context={
+        "people":people,
+    }
+    return render(request,"index.html",context)
